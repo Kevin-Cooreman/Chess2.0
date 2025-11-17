5ada5e723d1b77ec02290822490dc76681e833ae
Kevin-Cooreman <kevin.p.cooreman@gmail.com>
2025-11-17 22:41:08 +0100

    project initialised

---
+++ src/board.hpp
@@ -0,0 +1,12 @@
+// board header file
+// Defines the Board class and its member functions
+// using a bitboard representation for efficient game state management
+/* sources: https://www.chessprogramming.org/Bitboards 
+            https://ameye.dev/notes/chess-engine/
+*/
+
+#pragma once
+#include <iostream>
+using namespace std;
+
+
