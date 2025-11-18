7ad3a63f3379592d611d49d08dc22fc282e113b2
Kevin-Cooreman <kevin.p.cooreman@gmail.com>
2025-11-18 14:49:58 +0100

    precomputed move tables implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "board.hpp"
+#include "moveGeneration.hpp"
 #include <iostream>
 using namespace std;
 
@@ -10,5 +11,13 @@ int main(void){
     setupStartPos();
     
     printBoard();
+
+    for (int sq = 0; sq < 64; ++sq) {
+        uint64_t queen = RookMoves[sq] | BishopMoves[sq];
+        std::cout << "0x" << std::hex << queen << "ULL";
+        if (sq != 63) std::cout << ", ";
+        if ((sq + 1) % 8 == 0) std::cout << std::endl;
+    }
+    
     return 0;
 }
